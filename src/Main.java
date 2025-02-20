fa253418e9160234cd7f97c7a7b550d4a039b764
clifforddonk <clifforddonk@gmail.com>
2025-02-20 12:06:46 +0000

    Add Control Flow

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import com.Clifford.BMI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BMI.Calculator();
     }
 }
--- src/com/Clifford/BMI.java
+++ src/com/Clifford/BMI.java
@@ -6,21 +6,25 @@ public class BMI {
     public static void Calculator() {
         Scanner scanner=new Scanner(System.in);
 
-        int weight=0,height=0,index=0;
+        int weight=0,height=0;
         System.out.print("Enter your weight in kilograms(kg):");
         weight=scanner.nextInt();
         String status="Body Mass Index(BMI) status= ";
 
         System.out.println("Enter your height in meters(m):");
         height=scanner.nextInt();
-        index= weight/(height*height);
+       int index= weight/(height*height);
 
         System.out.println("Your Body Mass Index(BMI) is "+ index);
 
         if (index<18.5)
             System.out.println(status+ "Underweight");
-        else if (18.5<index=<25.0)
-            System.out.println(status+ "Normal");
+//        else if (18.5=<index<25.0)
+//            System.out.println(status+ "Normal");
+//        else if (25.0=<index<30)
+//            System.out.println(status+ "Overweight");
+//        else if (30=<index)
+//            System.out.println(status+ "Overweight");
 
 
 
